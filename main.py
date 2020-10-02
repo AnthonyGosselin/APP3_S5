3b487592f4d0af515c7d442800105b105c00780a
Test <gosa2506@usherbrooke.ca>
2020-10-02 11:37:21 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
+from scipy import signal
+
+
+class AudioSample:
+    def __init__(self, Fe, data):
+        self.Fe = Fe
+        self.data = data
+        self.N = len(data)
+        self.total_time =  1/Fe * self.N
+
+
+def load_audio(file):
+    Fe, data = wavfile.read(file)
+    return AudioSample(Fe, data)
+
+
+# Down sample beginning
+def down_sample(audioSample, samples=None, start_time=0, end_time=None, plot=True):
+
+    # Resolve parameters
+    samples = not samples and audioSample.N or samples
+    end_time = not end_time and 1/audioSample.Fe * audioSample.N or end_time
+    max_samples = int(audioSample.Fe * (end_time - start_time))
+    N2 = samples > max_samples and max_samples or samples
+    start_ind = int(start_time*audioSample.Fe)
+    end_ind = int(end_time*audioSample.Fe)
+
+    down_sample_data = signal.resample(audioSample.data[start_ind:end_ind], N2)
+    dn = (end_time - start_time) / N2
+    n = np.arange(start_time, end_time, dn)
+
+    if plot:
+        plt.figure(1)
+        plt.plot(n, down_sample_data)
+        plt.title('LA# (N: ' + str(N2) + '), time: ' + str(start_time) + 's - ' + str(end_time) + 's')
+        plt.show()
+
+    return down_sample_data
+
+
+guitarSample = load_audio('./audio/note_guitare_LAd.wav')
+down_sample(guitarSample)
